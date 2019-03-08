496bf447f85a5efc3ec7b25ff50e9ae9d8b4b437
oks4work <oks4work@gmail.com>
2019-03-08 15:07:59 +0900

    commit react

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 // index.js
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+
 import './sass/index.scss';
 
 const testFunction = () => {
 	[1, 2, 3].forEach(number => console.log(number));
 };
 
-console.log(testFunction);
-
 testFunction();
+
+ReactDOM.render(<App />, document.getElementById('root'));
