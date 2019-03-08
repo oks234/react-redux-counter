ff3c01276cdef88f8d41f3b5636bdcaae3c46b62
oks4work <oks4work@gmail.com>
2019-03-08 15:30:24 +0900

    created Counter components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Counter from "./Counter.js";
 
 class App extends React.Component {
 	render() {
 		return (
 			<div>
-				Counter
+				<Counter />
 			</div>
 		);
 	}
